25fb7c19868176bdc764673d3c5c8a16f6a85e25
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-09-11 00:39:13 +0200

    Parse header

--- unpak/main.cpp
+++ unpak/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+
+#include "pakdef.h"
+
+// Convert it to template?
+#define ASSERT_ERR(x, msg) \
+    if (!(x)) { \
+        std::cerr << msg << std::endl; \
+        exit(EXIT_FAILURE); \
+    }
 
 int main()
 {
-    std::cout << "TODO" << std::endl;
+    // TODO Make that clean
+    pak_header_t pakHeader;
+    std::ifstream inputfile("PAK0.PAK", std::ios::binary);
+    inputfile >> pakHeader.id >> pakHeader.offset >> pakHeader.size;
+
+    const int filesCount = pakHeader.size / sizeof(pak_file_t);
+    std::cout << "id: " << pakHeader.id
+        << "; offset: " << pakHeader.offset
+        << "; size: " << pakHeader.size
+        << "; nb files: " << filesCount
+        << std::endl;
+    ASSERT_ERR(std::memcmp(pakHeader.id, "PACK", 4) == 0, "Invalid PAK file!");
 }
