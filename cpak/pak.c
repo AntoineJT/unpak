8c1f7992f4d0ebcee926b12443693f5eba207b40
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-10-22 03:43:01 +0200

    Remove pak_load_file function

--- cpak/pak.c
+++ cpak/pak.c
@@ -1,5 +1,5 @@
-// Modified public domain unpak
-// implementation found from
+// Based on a public domain
+// implementation found at
 // https://quakewiki.org/wiki/.pak
 #include "pak.h"
 
@@ -8,67 +8,6 @@
 
 // TODO Implement LittleLong to convert endianness if needed
 #define LittleLong(x) x
-
-#if 0
-/* pak_filename : the os filename of the .pak file */
-/* filename     : the name of the file you're trying to load from the .pak file (remember to use forward slashes for the path) */
-/* out_filesize : if not null, the loaded file's size will be returned here */
-/* returns a malloced buffer containing the file contents (remember to free it later), or NULL if any error occurred */
-void* pak_load_file(const char* pak_filename, const char* filename, int* out_filesize)
-{
-  FILE* fp = fopen(pak_filename, "rb");
-  if (!fp)
-    return NULL;
-
-  pak_header_t pak_header;
-  if (!fread(&pak_header, sizeof(pak_header), 1, fp))
-    goto pak_error;
-  if (memcmp(pak_header.id, "PACK", 4) != 0)
-    goto pak_error;
-
-  pak_header.offset = LittleLong(pak_header.offset);
-  pak_header.size = LittleLong(pak_header.size);
-
-  int num_files = pak_header.size / sizeof(pak_file_t);
-
-  if (fseek(fp, pak_header.offset, SEEK_SET) != 0)
-    goto pak_error;
-
-  pak_file_t pak_file;
-  for (int i = 0; i < num_files; ++i)
-  {
-    if (!fread(&pak_file, sizeof(pak_file_t), 1, fp))
-      goto pak_error;
-
-    if (!strcmp(pak_file.name, filename))
-    {
-      pak_file.offset = LittleLong(pak_file.offset);
-      pak_file.size = LittleLong(pak_file.size);
-
-      if (fseek(fp, pak_file.offset, SEEK_SET) != 0)
-        goto pak_error;
-
-      void* buffer = malloc(pak_file.size);
-      if (!buffer)
-        goto pak_error;
-
-      if (!fread(buffer, pak_file.size, 1, fp))
-      {
-        free(buffer);
-        goto pak_error;
-      }
-
-      if (out_filesize)
-        *out_filesize = pak_file.size;
-      return buffer;
-    }
-  }
-
-pak_error:
-  fclose(fp);
-  return NULL;
-}
-#endif
 
 pak_files_t* pak_preload_files(const char* pak_filename)
 {
